e55a73d13da2aeac8e240f85ae13831c51c7be5c
Mani Saffarnia <32762080+ManiSaffarnia@users.noreply.github.com>
2023-04-15 11:58:12 +0330

    feat: add solution of problem number 14

---
+++ solutions/14-Longest-common-prefix.js
@@ -0,0 +1,64 @@
+/**
+ * Problem Description:
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Sample Inputs: 
+ * ["flower", "flow", "flight"]) => output: fl
+ * ["dog", "racecar", "car"]) => output: ""
+ * ["reflower", "flow", "flight"]) => => output: ""
+ * ["flower", "flower", "flower", "flower"]) => "flower"
+ */
+
+
+/**
+ * Solution number 1
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefixSolutionOne = function (strs) {
+  const longestPrefixResult = strs.reduce((longestPrefix, current) => {
+    for(let subStringlength=1; subStringlength<=current.length; subStringlength++){
+      const subStr = current.slice(0, subStringlength);
+      const hasThisPrefix = strs.every((item) => item.indexOf(subStr) === 0);
+      if (hasThisPrefix && subStr.length > longestPrefix.length) {
+        longestPrefix = subStr;
+      }
+    }
+
+    return longestPrefix;
+  }, "");
+
+  return longestPrefixResult;
+};
+
+/**
+ * Solution number 2
+ * This solution has a better performance
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefixSolutionTwo = function (strs) {
+  let longestPrefix = "";
+  for( let word of strs){
+    let shouldBreak = false;
+    for (let subStringlength = 1; subStringlength <= word.length; subStringlength++) {
+      const subStr = word.slice(0, subStringlength);
+      const hasThisPrefix = strs.every((item) => item.indexOf(subStr) === 0);
+      if (hasThisPrefix && subStr.length > longestPrefix.length) {
+        longestPrefix = subStr;
+      }
+      else {
+        shouldBreak = true;
+        break;
+      }
+    }
+  }
+
+  return longestPrefix;
+};
+
+module.exports = {
+  longestCommonPrefixSolutionOne,
+  longestCommonPrefixSolutionTwo
+};
