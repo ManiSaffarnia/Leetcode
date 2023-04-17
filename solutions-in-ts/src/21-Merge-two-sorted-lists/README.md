1100930ece03ea442430c7d837ae38a45c7b1aec
Mani Saffarnia <32762080+ManiSaffarnia@users.noreply.github.com>
2023-04-17 21:46:32 +0330

    feat: solution number 21 in TS

--- solutions-in-ts/src/21-Merge-two-sorted-lists/README.md
+++ solutions-in-ts/src/21-Merge-two-sorted-lists/README.md
@@ -1,30 +1,29 @@
 
 ## Difficulty: Easy
 ## Problem:
-Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
+You are given the heads of two sorted linked lists `list1` and `list2`.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
-You can return the answer in any order.
+Return the head of the merged linked list.
 
 **Only one valid answer exists.**
 
 ### Examples
 - Example one
 ```
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
 ```
 
 - Example Two 
 ```
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
+Input: list1 = [], list2 = []
+Output: []
 ```
 
 - Example Three 
 ```
-Input: nums = [3,3], target = 6
-Output: [0,1]
+Input: list1 = [], list2 = [0]
+Output: [0]
 ```
--- solutions-in-ts/src/21-Merge-two-sorted-lists/index.ts
+++ solutions-in-ts/src/21-Merge-two-sorted-lists/index.ts
@@ -1,11 +1,55 @@
-/**
- * @param {string} str
- * @return {string}
- */
-export const func = (str) => {
-
-
-  return str
+type NullableList = null | IListNode
+interface IListNode {
+  val: number | undefined,
+  next: NullableList
 }
 
-console.log(func(''));
+/* Definition for singly-linked list. */
+class ListNode implements IListNode {
+  constructor(public val: number | undefined, public next: NullableList = null) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+/** Solution Two - way better performance */
+export const  mergeTwoLists = (list1:NullableList = null, list2: NullableList = null): NullableList => {
+  const mergedList = new ListNode(0);
+
+  if(list1 === null && list2 !== null) {
+    return list2;
+  }
+  else if(list2 === null && list1 !== null){
+    return list1;
+  }
+  else if( list1 === null && list2 === null){
+    return null;
+  }
+  else if(list1?.val === 0 && list2?.val === 0 ){
+    return mergedList
+  }
+
+  const arrayOfList1 = []
+  while(list1!=null)
+  { 
+    arrayOfList1.push(list1.val);
+    list1 = list1.next;
+  }
+
+  const arrayOfList2 = []
+  while(list2!=null)
+  { 
+    arrayOfList2.push(list2.val);
+    list2 = list2.next;
+  }
+
+  const mergedArray = arrayOfList1.concat(arrayOfList2).sort((a, b) => {return (a!-b!)});
+  
+  let temp = mergedList;
+  for(let i of mergedArray){
+    temp.next = new ListNode(i);
+    temp = temp.next;
+  }
+
+  return mergedList.next;
+};
