a4063da185a73b097dbb6ccb8bbb72bc8c624de9
Mani Saffarnia <32762080+ManiSaffarnia@users.noreply.github.com>
2023-04-17 21:46:55 +0330

    feat: add second solution to js and add test

--- solutions-in-js/src/21-Merge-two-sorted-lists/index.js
+++ solutions-in-js/src/21-Merge-two-sorted-lists/index.js
@@ -20,7 +20,7 @@ const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 // const list2 = new ListNode(1, null);
 
 
-var mergeTwoLists = function (list1 = null, list2 = null) {
+const mergeTwoListsSolutionOne = function (list1 = null, list2 = null) {
   let mergedList = new ListNode(0);
   let mergedListTail = mergedList;
   let item1 = list1;
@@ -109,11 +109,53 @@ var mergeTwoLists = function (list1 = null, list2 = null) {
     }
   }
 
-  return  mergedList.next;;
+  return  mergedList.next;
 };
 
-// console.log(JSON.stringify(mergeTwoLists(list1, list2)));
+const mergeTwoListsSolutionTwo = function (list1, list2) {
+  const mergedList = new ListNode(0);
 
-module.export = {
-  mergeTwoLists
+  if(list1 === null && list2 !== null) {
+    return list2;
+  }
+  else if(list2 === null && list1 !== null){
+    return list1;
+  }
+  else if( list1 === null && list2 === null){
+    return null;
+  }
+  else if(list1?.val === 0 && list2?.val === 0 ){
+    return mergedList
+  }
+
+  const arrayOfList1 = []
+  while(list1!=null)
+  { 
+    arrayOfList1.push(list1.val);
+    list1 = list1.next;
+  }
+
+  const arrayOfList2 = []
+  while(list2!=null)
+  { 
+    arrayOfList2.push(list2.val);
+    list2 = list2.next;
+  }
+
+  const mergedArray = arrayOfList1.concat(arrayOfList2).sort((a, b) => {return (a-b)});
+  
+  let temp = mergedList;
+  for(let i of mergedArray){
+    temp.next = new ListNode(i);
+    temp = temp.next;
+  }
+
+  return mergedList.next;
+}
+
+
+module.exports = {
+  mergeTwoListsSolutionOne,
+  mergeTwoListsSolutionTwo,
+  ListNode
 }
