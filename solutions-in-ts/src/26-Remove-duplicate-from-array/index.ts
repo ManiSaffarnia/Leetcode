31227326532a0a74093ae9f54bc4a6f0c61f7973
Mani Saffarnia <32762080+ManiSaffarnia@users.noreply.github.com>
2023-04-18 22:53:57 +0330

    feat: add solution for problem number 26 in TS

--- solutions-in-ts/src/26-Remove-duplicate-from-array/index.ts
+++ solutions-in-ts/src/26-Remove-duplicate-from-array/index.ts
@@ -1,11 +1,29 @@
 /**
- * @param {string} str
- * @return {string}
+ * @param {number[]} nums
+ * @return {number}
  */
-export const func = (str) => {
+export const removeDuplicatesSolutionOne = (nums: number[]):number => {
+  let uniqueCount = 1;
 
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[uniqueCount] = nums[i];
+      uniqueCount++;
+      continue;
+    }
+  }
 
-  return str
+  return uniqueCount;
+
 }
 
-console.log(func(''));
+export const removeDuplicatesSolutionTwo = (nums: number[]): number => {
+  for (let i = 1; i <= nums.length-1; i++) {
+
+    if(nums[i-1] === nums[i]){
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
